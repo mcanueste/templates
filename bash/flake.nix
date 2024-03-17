59acfa79dbca8a85537fd6db74c891213b860cc2
mcanueste <mcanueste@gmail.com>
2024-03-17 18:08:47 +0100

    use flake-parts for the bash template

--- bash/flake.nix
+++ bash/flake.nix
@@ -1,54 +1,53 @@
 {
   description = "Example Bash application project.";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
-  outputs = {
-    self,
+  outputs = inputs @ {
     nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      debug = true;
+      imports = [];
+      flake = {};
+      perSystem = {
+        self',
+        inputs',
+        system,
+        pkgs,
+        config,
+        ...
+      }: let
+        name = "bash-hello";
+        lastModifiedDate = self'.lastModifiedDate or self'.lastModified or "19700101";
+        version = builtins.substring 0 8 lastModifiedDate;
+      in {
+        packages = {
+          default = pkgs.writeShellApplication {
+            inherit name;
+            runtimeInputs = with pkgs; [coreutils];
+            text = ''
+              echo "Hello, world!"
+            '';
+          };
 
-    # to work with older version of flakes
-    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-    version = builtins.substring 0 8 lastModifiedDate;
-    name = "bash-hello";
-  in {
-    devShells = forAllSystems (system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          beautysh
-          shellharden
-          shellcheck
-        ];
-      };
-    });
-
-    packages = forAllSystems (system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      default = self.packages.${system}.${name};
-      bash-hello = pkgs.writeShellApplication {
-        inherit name;
-        runtimeInputs = with pkgs; [coreutils];
-        text = ''
-          echo "Hello, world!"
-        '';
-      };
-      docker = pkgs.dockerTools.buildImage {
-        inherit name;
-        tag = version;
-        config = {
-          Cmd = ["${self.packages.${system}.default}/bin/${name}"];
-          Env = [
-            "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-          ];
+          docker = pkgs.dockerTools.buildImage {
+            inherit name;
+            tag = version;
+            config = {
+              Cmd = ["${self'.packages.default}/bin/${name}"];
+              Env = [
+                "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              ];
+            };
+          };
         };
       };
-    });
-  };
+    };
 }
